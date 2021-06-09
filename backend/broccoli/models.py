17d24b96781883372f61af039ce41948208caa8f
antoniouaa <antoniouaa@hotmail.com>
2021-06-09 14:58:28 +0100

    savepoint for migrations lmao

--- backend/broccoli/models.py
+++ backend/broccoli/models.py
@@ -1,7 +1,17 @@
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+from sqlalchemy.sql.schema import ForeignKey, Table
 
 from broccoli.db import Base
+
+
+association_table = Table(
+    "association",
+    Base.metadata,
+    Column("item_id", Integer, ForeignKey("items.id")),
+    Column("entry_id", Integer, ForeignKey("entries.id")),
+)
 
 
 class Item(Base):
@@ -21,3 +31,12 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     created_at = Column(DateTime(timezone=True), default=func.now())
+    entry_id = Column(Integer, ForeignKey("entries.id"))
+    entry = relationship("Entry", backref="users")
+
+
+class Entry(Base):
+    __tablename__ = "entries"
+
+    id = Column(Integer, primary_key=True, index=True)
+    items = relationship("Item", secondary=association_table, backref="entries")
