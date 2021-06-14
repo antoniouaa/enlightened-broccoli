e4d00c7b815a8073ef75772265de8039c690b479
antoniouaa <antoniouaa@hotmail.com>
2021-06-14 15:35:29 +0100

    deleting items no longer supported

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -1,51 +1,6 @@
 import json
 
-from tests.conftest import test_items, test_users
-
-
-def test_item_creation_success(test_client):
-    chicken, milk = test_items
-
-    response = test_client.post("/items/", data=json.dumps(chicken))
-    assert response.status_code == 201
-    assert response.json()["title"] == "chicken"
-
-    response = test_client.post("/items/", data=json.dumps(milk))
-    assert response.status_code == 201
-    assert response.json()["title"] == "milk"
-
-    no_description = {"title": "test title", "calories": 0}
-
-    response = test_client.post("/items/", data=json.dumps(no_description))
-    assert response.status_code == 201
-
-    response = test_client.get("/items/")
-    assert len(response.json()) == 3
-
-
-def test_item_creation_failure(test_client):
-    bad_title = {"title": None, "description": "test", "calories": 0}
-
-    response = test_client.post("/items/", data=json.dumps(bad_title))
-    assert response.status_code == 422
-
-    bad_description = {"title": "test title", "description": 10, "calories": 0}
-
-    response = test_client.post("/items/", data=json.dumps(bad_description))
-    assert response.status_code == 422
-
-    bad_calories = {
-        "title": "test title",
-        "description": "test description",
-        "calories": None,
-    }
-
-    response = test_client.post("/items/", data=json.dumps(bad_calories))
-    assert response.status_code == 422
-
-    response = test_client.get("/items/")
-    assert response.status_code == 200
-    assert len(response.json()) == 0
+from tests.conftest import test_users
 
 
 def test_user_creation_success(test_client):
